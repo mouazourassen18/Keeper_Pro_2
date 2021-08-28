c0353df448f6affd8892fae9c4735caa5bb64e90
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-08-28 07:48:22 -0400

    Final update

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "/src/notes.js";
+
+function createCard(pro) {
+  return <Note key={pro.id} title={pro.title} content={pro.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createCard)}
       <Footer />
     </div>
   );
